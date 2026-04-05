8d78caf9f73900d47df01eb49c2376ddb0695b81
Yan Minagawa <yan@minagawa.de>
2026-04-05 10:11:37 +0700

    manifestor v0.1.3.0

--- Formula/manifestor.rb
+++ Formula/manifestor.rb
@@ -1,8 +1,8 @@
 class Manifestor < Formula
   desc "Grab the latest file from ~/Downloads with a clean name"
   homepage "https://github.com/yncyrydybyl/manifestor"
-  url "https://github.com/yncyrydybyl/manifestor/archive/refs/tags/v0.1.1.0.tar.gz"
-  sha256 "35472166abc15d4048f5890a26dccd3d6101f3aac67ce35be9770046993d7b62"
+  url "https://github.com/yncyrydybyl/manifestor/archive/refs/tags/v0.1.3.0.tar.gz"
+  sha256 "9a259fdad343e1d0124ccf210aa8d018c05bb30ed432fc3d90132d6cae2d1a5e"
   license "MIT"
 
   depends_on "go" => :build
@@ -13,9 +13,12 @@ class Manifestor < Formula
            "-o", bin/"m",
            "./cmd/m"
     bin.install_symlink "m" => "mm"
+
+    generate_completions_from_executable(bin/"m", "completion")
   end
 
   test do
     assert_match version.to_s, shell_output("#{bin}/m --version")
+    assert_match "COMP_WORDS", shell_output("#{bin}/m completion bash")
   end
 end
